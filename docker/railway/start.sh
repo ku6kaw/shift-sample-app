daaabbe098d3e7dbe9515c8f3cf8f0b04b66d5b9
ku6kaw <reoreo01.123@gmail.com>
2025-10-17 01:47:27 +0900

    edit dockerfile

--- docker/railway/start.sh
+++ docker/railway/start.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 set -e
 
+if [ "${DB_HOST}" = '${MYSQLHOST}' ] && [ -n "${MYSQLHOST}" ]; then
+    export DB_HOST="${MYSQLHOST}"
+fi
+
+if [ "${DB_PORT}" = '${MYSQLPORT}' ] && [ -n "${MYSQLPORT}" ]; then
+    export DB_PORT="${MYSQLPORT}"
+fi
+
+if [ "${DB_DATABASE}" = '${MYSQLDATABASE}' ] && [ -n "${MYSQLDATABASE}" ]; then
+    export DB_DATABASE="${MYSQLDATABASE}"
+fi
+
+if [ "${DB_USERNAME}" = '${MYSQLUSER}' ] && [ -n "${MYSQLUSER}" ]; then
+    export DB_USERNAME="${MYSQLUSER}"
+fi
+
+if [ "${DB_PASSWORD}" = '${MYSQLPASSWORD}' ] && [ -n "${MYSQLPASSWORD}" ]; then
+    export DB_PASSWORD="${MYSQLPASSWORD}"
+fi
+
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
